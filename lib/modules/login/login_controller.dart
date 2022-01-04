98b7923517d60f35caed267a9178af8814493ee5
Guilherme H. Discher <discher.guilherme@hotmail.com>
2022-01-03 21:20:35 -0300

    Aula #03: Autenticação

---
+++ lib/modules/login/login_controller.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:payflow/shared/auth/auth_controller.dart';
+
+class LoginController {
+  final auth_controller = AuthController();
+
+  Future<void> googleSignIn(BuildContext context) async {
+    GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ["email"]);
+
+    try {
+      final response = await _googleSignIn.signIn();
+      auth_controller.SetUser(context, response);
+      print(response);
+    } catch (error) {
+      print(error);
+    }
+  }
+}
