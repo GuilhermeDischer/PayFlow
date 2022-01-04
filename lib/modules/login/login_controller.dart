20c98a4938bc49990c40b9fb6434acce67d6cac6
Guilherme H. Discher <discher.guilherme@hotmail.com>
2022-01-03 21:56:49 -0300

    Aula #03: Melhorias na Autenticação

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:payflow/shared/auth/auth_controller.dart';
+import 'package:payflow/shared/models/user_model.dart';
 
 class LoginController {
   final auth_controller = AuthController();
@@ -10,7 +11,12 @@ class LoginController {
 
     try {
       final response = await _googleSignIn.signIn();
-      auth_controller.SetUser(context, response);
+      final user = UserModel(
+        name: response!.displayName!, 
+        photoURL: response.photoUrl
+      );
+      
+      auth_controller.SetUser(context, user);
       print(response);
     } catch (error) {
       print(error);
